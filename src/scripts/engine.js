07fcd8a41c38840a62f30c772d21d2f2a766cb49
lucasmbc <lucasmbc@hotmail.com>
2023-12-10 17:59:27 -0300

    adiciona cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,29 @@
+const emojis = [
+    "😺",
+    "😺",
+    "🐒",
+    "🐒",
+    "🐮",
+    "🐮",
+    "🐭",
+    "🐭",
+    "🦏",
+    "🦏",
+    "🐯",
+    "🐯",
+    "🦁",
+    "🦁",
+    "🐶",
+    "🐶",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < shuffleEmojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
